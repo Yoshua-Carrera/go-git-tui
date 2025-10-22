5914ec84504b5bbb99c66dc5a301f080667dd5a1
Yoshua Carrera Vargas <yoshua.carrera24@gmail.com>
2025-10-22 13:28:03 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"log"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Styles struct {
+	BorderColor lipgloss.Color
+	InputField  lipgloss.Style
+}
+
+func DefaultStyles() *Styles {
+	s := new(Styles)
+	s.BorderColor = lipgloss.Color("36")
+	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
+
+	return s
+}
+
+type model struct {
+	questions   []string
+	index       int
+	width       int
+	height      int
+	styles      *Styles
+	answerField textinput.Model
+}
+
+func New(questions []string) *model {
+	styles := DefaultStyles()
+	answerField := textinput.New()
+	answerField.Placeholder = "Your answer here"
+	return &model{questions: questions, answerField: answerField, styles: styles}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	if m.width == 0 {
+		return "loading..."
+	}
+	return lipgloss.JoinVertical(lipgloss.Center, m.questions[m.index], m.styles.InputField.Render(m.answerField.View()))
+}
+
+func main() {
+	questions := []string{"test1?", "test2"}
+	m := New(questions)
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+	defer f.Close()
+	p := tea.NewProgram(m, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
